9f180c39a374d7ed1dd8c397642b4f207bde0b45
Roy van de Water <roy.v.water@gmail.com>
2011-01-01 14:09:55 -0700

    Updated setup script to pull down plugins and create backups

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+echo "Pulling down git submodules for vim plugins"
+git submodule init
+git submodule update
+
+echo "Backing up existing configuration files to $HOME/.vimbackup/"
+mv ~/.vim ~/.vimbackup || echo "~/.vim not found, nothing to backup. Creating empty ~/.vimbackup" && mkdir ~/.vimbackup
+mv ~/.vimrc ~/.vimbackup/ || echo "~/.vimrc not found, nothing to backup"
+mv ~/.gvimrc ~/.vimbackup/ || echo "~/.gvimrc not found, nothing to backup"
+
+echo "Installing new vim configuration files"
 ln -s $PWD ~/.vim
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/gvimrc ~/.gvimrc
