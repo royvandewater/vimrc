28e71a94c158c75d0c9816afb6a76fd565195467
Roy van de Water <roy.v.water@gmail.com>
2011-01-01 14:30:12 -0700

    Added color to the setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-echo "Pulling down git submodules for vim plugins"
+red () {
+  echo -e "\033[1;31m$1\033[0m"
+}
+
+green () {
+  echo -e "\033[1;32m$1\033[0m"
+}
+
+green "Pulling down git submodules for vim plugins"
 git submodule init
 git submodule update
 
-echo "Backing up existing configuration files to $HOME/.vimbackup/"
-mv ~/.vim ~/.vimbackup || echo "~/.vim not found, nothing to backup. Creating empty ~/.vimbackup" && mkdir ~/.vimbackup
-mv ~/.vimrc ~/.vimbackup/ || echo "~/.vimrc not found, nothing to backup"
-mv ~/.gvimrc ~/.vimbackup/ || echo "~/.gvimrc not found, nothing to backup"
+green "Backing up existing configuration files to $HOME/.vimbackup/"
+mv ~/.vim ~/.vimbackup || (red "~/.vim not found, nothing to backup. Creating empty ~/.vimbackup" && mkdir ~/.vimbackup)
+mv ~/.vimrc ~/.vimbackup/ || red "~/.vimrc not found, nothing to backup"
+mv ~/.gvimrc ~/.vimbackup/ || red "~/.gvimrc not found, nothing to backup"
 
-echo "Installing new vim configuration files"
+green "Installing new vim configuration files"
 ln -s $PWD ~/.vim
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/gvimrc ~/.gvimrc
